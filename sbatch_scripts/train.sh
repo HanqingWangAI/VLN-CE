ab51f360275ce96e9dc177ec08812aa6a6da5938
hanqingwangai <hanqingwang@bit.edu.cn>
2022-01-02 00:19:45 +0100

    update

--- sbatch_scripts/train.sh
+++ sbatch_scripts/train.sh
@@ -9,5 +9,6 @@ export MAGNUM_LOG=quiet
 bsub -n 16 -R "rusage[mem=4000,ngpus_excl_p=8]" -W 48:00 -oo logs python -u -m torch.distributed.launch \
     --use_env \
     --nproc_per_node 8 \
+    run.py \
     --exp-config vlnce_baselines/config/r2r_waypoint/5-hpn-_c.yaml \
     --run-type train
