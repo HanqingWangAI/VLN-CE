3192174baaf29934069df7088dd400755589567b
hanqingwangai <hanqingwang@bit.edu.cn>
2022-01-20 22:21:04 +0100

    update

--- sbatch_scripts/train_cma.sh
+++ sbatch_scripts/train_cma.sh
@@ -6,7 +6,6 @@ export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
 
 
-bsub -n 8 -R "rusage[mem=8000,ngpus_excl_p=1]" -W 48:00 -oo logs python 
-    run.py \
+bsub -n 8 -R "rusage[mem=8000,ngpus_excl_p=1]" -W 48:00 -oo logs python run.py \
     --exp-config vlnce_baselines/config/r2r_baselines/cma.yaml \
     --run-type train
