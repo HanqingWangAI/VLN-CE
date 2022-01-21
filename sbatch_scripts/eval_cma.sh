55208af746f1c7c13d1ce3e25cbcc02b1c101c94
hanqingwangai <hanqingwang@bit.edu.cn>
2022-01-21 10:21:57 +0100

    eval

---
+++ sbatch_scripts/eval_cma.sh
@@ -0,0 +1,11 @@
+cd ..
+
+# alias python=/cluster/home/hanwang/miniconda3/envs/habitat/bin/python
+
+export GLOG_minloglevel=2
+export MAGNUM_LOG=quiet
+
+
+bsub -n 8 -R "rusage[mem=8000,ngpus_excl_p=1]" -W 48:00 -oo logs python run.py \
+    --exp-config vlnce_baselines/config/r2r_baselines/cma.yaml \
+    --run-type eval
