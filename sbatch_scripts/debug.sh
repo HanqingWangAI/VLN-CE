ddac43f648a05e2edb242e5692ae5c901939e612
hanqingwangai <hanqingwang@bit.edu.cn>
2022-01-24 18:03:54 +0100

    update

--- sbatch_scripts/debug.sh
+++ sbatch_scripts/debug.sh
@@ -6,7 +6,14 @@ export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
 
 
-bsub -n 4 -R "rusage[mem=8000,ngpus_excl_p=1]" -R "select[gpu_model0==NVIDIATITANRTX]" -W 1:00 -oo logs python -u -m torch.distributed.launch \
+# bsub -n 4 -R "rusage[mem=8000,ngpus_excl_p=1]" -R "select[gpu_model0==NVIDIATITANRTX]" -W 1:00 -oo logs python -u -m torch.distributed.launch \
+#     --use_env \
+#     --nproc_per_node 1 \
+#     run.py \
+#     --exp-config vlnce_baselines/config/r2r_waypoint/6-hpn-__.yaml \
+#     --run-type train
+
+bsub -n 4 -R "rusage[mem=8000,ngpus_excl_p=1]" -W 1:00 -oo logs python -u -m torch.distributed.launch \
     --use_env \
     --nproc_per_node 1 \
     run.py \
