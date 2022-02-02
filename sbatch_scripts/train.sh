9052b96e843b272832b13750644d266de5ec5717
hanqingwangai <hanqingwang@bit.edu.cn>
2022-02-02 22:24:23 +0100

    update

--- sbatch_scripts/train.sh
+++ sbatch_scripts/train.sh
@@ -6,7 +6,7 @@ export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
 
 
-bsub -n 16 -R "rusage[mem=8000,ngpus_excl_p=4]" -R "select[gpu_model0==NVIDIATITANRTX]" -W 72:00 -oo logs python -u -m torch.distributed.launch \
+bsub -n 16 -R "rusage[mem=8000,ngpus_excl_p=4]" -R "select[gpu_model0==NVIDIATITANRTX]" -W 48:00 -oo logs python -u -m torch.distributed.launch \
     --use_env \
     --nproc_per_node 4 \
     run.py \
