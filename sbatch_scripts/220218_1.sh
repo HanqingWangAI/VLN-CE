c7dabb18bcb34dda76bfb2beec5464e25ef35bc0
hanqingwangai <hanqingwang@bit.edu.cn>
2022-02-18 21:49:33 +0100

    update

---
+++ sbatch_scripts/220218_1.sh
@@ -0,0 +1,14 @@
+# cd ..
+
+# alias python=/cluster/home/hanwang/miniconda3/envs/habitat/bin/python
+
+export GLOG_minloglevel=2
+export MAGNUM_LOG=quiet
+
+
+python -u -m torch.distributed.launch \
+    --use_env \
+    --nproc_per_node 4 \
+    run.py \
+    --exp-config vlnce_baselines/config/r2r_waypoint/6.yaml \
+    --run-type train
