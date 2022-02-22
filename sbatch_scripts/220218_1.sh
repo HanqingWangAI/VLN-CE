d6a042958c295c9bcd23082739c90d5805958745
hanqingwangai <hanqingwang@bit.edu.cn>
2022-02-22 21:12:06 +0100

    update

--- sbatch_scripts/220218_1.sh
+++ sbatch_scripts/220218_1.sh
@@ -9,7 +9,6 @@ export MAGNUM_LOG=quiet
 python -u -m torch.distributed.launch \
     --use_env \
     --nproc_per_node 4 \
-    # --master_port 61501 \
     run.py \
     --exp-config vlnce_baselines/config/r2r_waypoint/7.yaml \
     --run-type train
